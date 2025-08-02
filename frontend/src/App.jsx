f5d3fcb7cb3f5e65d1a7483e4cdb7ee709219cd8
Aditya Kumar <ashutosh754194@gmail.com>
2025-08-02 21:32:47 +0530

    frontend app file changed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -16,22 +16,27 @@ import OurCourses from './admin/OurCourses'
 
 
 const App = () => {
+  const user = JSON.parse(localStorage.getItem("user"));
+  const admin = JSON.parse(localStorage.getItem("admin"));
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>}/>
-        <Route path='/login' element={<Login/>}/>
-        <Route path='/signup' element={<Signup/>}/>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/signup' element={<Signup />} />
 
 
-        <Route path='/courses' element={<Courses/>}/>
-        <Route path='/buy/:courseId' element={<Buy/>}/>
-        <Route path='purchases' element={<Purchases/>}/>
+        <Route path='/courses' element={<Courses />} />
+        <Route path='/buy/:courseId' element={<Buy />} />
+        <Route path='purchases' element={<Purchases />} />
 
         {/* Admin Routes */}
         <Route path="/admin/signup" element={<AdminSignup />} />
         <Route path="/admin/login" element={<AdminLogin />} />
-        <Route path="/admin/dashboard" element={<Dashboard />} />
+        <Route
+          path="/admin/dashboard"
+          element={admin ? <Dashboard /> : <Navigate to={"/admin/login"} />}
+        />
         <Route path="/admin/create-course" element={<CourseCreate />} />
         <Route path="/admin/update-course/:id" element={<UpdateCourse />} />
         <Route path="/admin/our-courses" element={<OurCourses />} />
